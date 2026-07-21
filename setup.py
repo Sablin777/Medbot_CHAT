07e913eb315da3ef84888b6f3eb4aae8ddf3a495
Ilya Sablin <sablin_27@mail.ru>
2026-07-21 15:45:30 +0400

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+name = "Medbot_CHAT",
+version= "0.1.0",
+author="ILSAB",
+author_email ="***",
+packages=find_packages(),
+install_requires=[]
+)
